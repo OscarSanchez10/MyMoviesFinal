c31502f8c0f7a0f3efd8c1ccef625faca7913251
Oscar Sanchez <129460323+OscarSanchez10@users.noreply.github.com>
2024-05-21 12:25:09 -0700

    Update context_processors.py

--- movies/context_processors.py
+++ movies/context_processors.py
@@ -1,6 +1,10 @@
 from django.contrib.auth.models import User
+# Importa el modelo de usuario predeterminado de Django.
 
 def user_context(request):
+    # Define una función llamada user_context que toma una solicitud (request) como argumento.
+
     return {
         'user_name': request.user.username if request.user.is_authenticated else 'Guest'
     }
+    # Devuelve un diccionario con el nombre de usuario del usuario actual si está autenticado, de lo contrario, devuelve 'Guest'.
