fcf5106bf29f2abc656862d496f4fd8239ea9f45
Oscar Sanchez <129460323+OscarSanchez10@users.noreply.github.com>
2024-05-21 12:24:47 -0700

    Update admin.py

--- movies/admin.py
+++ movies/admin.py
@@ -1,10 +1,23 @@
 from django.contrib import admin
+# Importa el módulo de administración de Django.
 
 from movies.models import Movie, Genre, Job, Person, MovieCredit, MovieReview 
+# Importa los modelos del módulo 'movies.models'.
 
 admin.site.register(Movie)
+# Registra el modelo 'Movie' en el panel de administración.
+
 admin.site.register(Genre)
+# Registra el modelo 'Genre' en el panel de administración.
+
 admin.site.register(Job)
+# Registra el modelo 'Job' en el panel de administración.
+
 admin.site.register(Person)
+# Registra el modelo 'Person' en el panel de administración.
+
 admin.site.register(MovieCredit)
+# Registra el modelo 'MovieCredit' en el panel de administración.
+
 admin.site.register(MovieReview)
+# Registra el modelo 'MovieReview' en el panel de administración.
